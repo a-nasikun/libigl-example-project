2239a5300e6a6690c09682b160391c0baca51d42
Ahmad Nasikun <nasikun.jpr@gmail.com>
2024-08-13 18:12:45 +0700

    Util: setup initial system

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <igl/opengl/glfw/Viewer.h>
 
+// Bartels
+#include <linear_tet_dphi_dX.h>
+#include <linear_tri2d_dphi_dX.h>
+#include <linear_tri2dmesh_dphi_dX.h>
+#include <linear_tri2dmesh_corotational_dq.h>
+#include <linear_tetmesh_neohookean_dq2.h>
+#include <assemble.h>
+#include <flatten.h>
+#include <flatten_multiply.h>
+#include <eval_at_point.h>
+#include <linear_tetmesh_dphi_dX.h>
+#include <simple_psd_fix.h>
+
 int main(int argc, char *argv[])
 {
   // Inline mesh of a cube
-  const Eigen::MatrixXd V= (Eigen::MatrixXd(8,3)<<
+  /*const Eigen::MatrixXd V = (Eigen::MatrixXd(8, 3) <<
     0.0,0.0,0.0,
     0.0,0.0,1.0,
     0.0,1.0,0.0,
@@ -24,11 +37,21 @@ int main(int argc, char *argv[])
     0,4,5,
     0,5,1,
     1,5,7,
-    1,7,3).finished();
+    1,7,3).finished();*/
+
+    Eigen::MatrixXd V, dphidX, X;
+    Eigen::MatrixXi E;
+    Eigen::MatrixXi F;
+    Eigen::VectorXd v; //volumes
+
+    igl::readMESH("../Bartels/data/meshes_mesh/coarser_bunny.mesh", V, E, F);
+    std::cout << "V" << V.cols() << "x" << V.rows() << std::endl;
+    std::cout << "E" << E.cols() << "x" << E.rows() << std::endl;
+    std::cout << "F" << F.cols() << "x" << F.rows() << std::endl;
 
   // Plot the mesh
   igl::opengl::glfw::Viewer viewer;
-  viewer.data().set_mesh(V, F);
+  viewer.data().set_mesh(V, E);
   viewer.data().set_face_based(true);
   viewer.launch();
 }
